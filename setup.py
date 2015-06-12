e35063c1f56f1d372e1fd7ea43d5918dfebf66d5
Alexey Dubkov <alexey.dubkov@elastica.co>
2015-06-11 19:32:40 -0700

    Make cleanup before applying.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='zabbixcli',
-      version = '1.0.1',
+      version = '1.0.2',
       description = 'Tool for manage zabbix templates as YAML files.',
       author = 'Alexey Dubkov',
       author_email = 'alexey.dubkov@gmail.com',
       packages = find_packages(),
       scripts = ['zabbixcli','zabbixcli-worker'],
-      install_requires = ["argparse", "py-zabbix"],
+      install_requires = ["argparse", "py-zabbix>=0.5.6"],
       url = 'https://github.com/blacked/zabbixcli',
      )
